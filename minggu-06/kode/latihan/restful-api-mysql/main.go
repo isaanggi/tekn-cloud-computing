06cd209c6300efc60d1162880187d15f1109fdc3
Isa Anggi <112955252+isaanggi@users.noreply.github.com>
2022-10-16 10:38:50 +0700

    Update main.go

--- minggu-06/kode/latihan/restful-api-mysql/main.go
+++ minggu-06/kode/latihan/restful-api-mysql/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:isaanggi90@tcp(127.0.0.1:3306)/gotest")
+	db, err := sql.Open("mysql", "root:<password>@tcp(127.0.0.1:3306)/gotest")
 	if err != nil {
 		fmt.Print(err.Error())
 	}
